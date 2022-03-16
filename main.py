483431b812bcb2a7987859e3be3e65b52e8ff949
martaweronikam <101061129+martaweronikamw@users.noreply.github.com>
2022-03-16 19:14:05 +0000

    first function

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import numpy as np
 import os
 import random
 import matplotlib.pyplot as plt
+from scipy import stats
+
+isMale = random.randint(0,1)
+
+def genAge(isMale): 
+	if isMale==0:
+		stats.norm.rvs(50, 15) 
+	else:
+		stats.norm.rvs(55,18)
 
 for num in range(100):
     user_id = num
-    sex = ""
-    age = ""
+    sex = isMale
+    age = genAge(sex)
     condition = ""
     
+
 
